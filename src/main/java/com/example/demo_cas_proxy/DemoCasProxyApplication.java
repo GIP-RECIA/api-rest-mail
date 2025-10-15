17b151cb585f3f31f520be636c4b25a286b562da
Ewann ROUX <ewann.roux@recia.fr>
2025-10-15 10:19:09 +0200

    initial commit web controller ok

---
+++ src/main/java/com/example/demo_cas_proxy/DemoCasProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_cas_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCasProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCasProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_cas_proxy/DemoCasProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_cas_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCasProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
