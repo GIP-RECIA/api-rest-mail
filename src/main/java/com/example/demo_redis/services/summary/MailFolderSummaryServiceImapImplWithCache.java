1c4b9fc90901fece5a9f430b25a3fe905bff27ab
Ewann ROUX <ewann.roux@recia.fr>
2025-10-28 11:27:31 +0100

    fix: remove MailFolderSummaryServiceImapImplWithCache implemented cache logic already defined in its abstract parent class

--- src/main/java/com/example/demo_redis/services/summary/MailFolderSummaryServiceImapImplWithCache.java
+++ src/main/java/com/example/demo_redis/services/summary/MailFolderSummaryServiceImapImplWithCache.java
@@ -44,35 +44,7 @@ public class MailFolderSummaryServiceImapImplWithCache extends AbstractMailFolde
 
     @Override
     protected MailFolderSummaryForWidget getMailFolderSummaryForWidgetWithoutCache(Principal principal) throws MessagingException {
-        Cache cache = cacheManager.getCache(redisProperties.getResponseCacheName());
-        String cacheKey = principal.getName();
-
-
-        if (cache == null){
-            throw new IllegalStateException(String.format("Can't get cache %s", redisProperties.getResponseCacheName()));
-        }
-
-        try {
-            Optional<MailFolderSummaryForWidget> summaryFromCache = Optional.ofNullable(cache.get(cacheKey, MailFolderSummaryForWidget.class));
-            if (summaryFromCache.isPresent()) {
-                log.debug("Return value from cache");
-                return summaryFromCache.get();
-            }
-        } catch (Exception e) {
-            log.error("Could not load cache for {}", principal, e);
-            cache.evict(cacheKey);
-        }
-
-        MailFolderSummaryForWidget result = getFromImap(principal);
-
-        try {
-            cache.put(cacheKey, result);
-        } catch (Exception e) {
-            log.error("Could not write cache for {}, evicting key from cache, response will be returned", principal, e);
-            cache.evict(cacheKey);
-        }
-
-        return result;
+        return getFromImap(principal);
     }
 
 
