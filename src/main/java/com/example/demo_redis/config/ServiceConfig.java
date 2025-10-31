c417902b2b6c73c37695ee8c76773902c234aea6
Ewann ROUX <ewann.roux@recia.fr>
2025-10-31 10:47:44 +0100

    fix: use  ConditionalOnProperty instead of Profile for mock service

--- src/main/java/com/example/demo_redis/config/ServiceConfig.java
+++ src/main/java/com/example/demo_redis/config/ServiceConfig.java
@@ -5,15 +5,15 @@ import com.example.demo_redis.services.summary.MailFolderSummaryServiceImapImplW
 import com.example.demo_redis.services.summary.MailFolderSummaryServiceMockWithCache;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Slf4j
 @Configuration
 public class ServiceConfig {
     @Bean(name = "mailFolderSummary")
-    @Profile("mock")
+    @ConditionalOnProperty(prefix = "mock", name = "enable", havingValue = "true")
     public IMailFolderSummaryService getServiceMock(){
         log.info("create mock bean for IMailFolderSummaryService");
         return new MailFolderSummaryServiceMockWithCache();
