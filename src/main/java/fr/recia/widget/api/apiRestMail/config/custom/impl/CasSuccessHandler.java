f3be12df6a7626e6ffb032ebd67d3e5cbb5d56e7
Ewann ROUX <ewann.roux@recia.fr>
2025-12-15 11:33:04 +0100

    fix: fix inceptionYear for license header

--- src/main/java/fr/recia/widget/api/apiRestMail/config/custom/impl/CasSuccessHandler.java
+++ src/main/java/fr/recia/widget/api/apiRestMail/config/custom/impl/CasSuccessHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © ${project.inceptionYear} GIP-RECIA (https://www.recia.fr/)
+ * Copyright © 2025 GIP-RECIA (https://www.recia.fr/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package fr.recia.widget.api.apiRestMail.config.custom.impl;
 
 import lombok.extern.slf4j.Slf4j;
